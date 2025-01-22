64d505b920ae2230d07a03a504642f5e99028ceb
Jaloliddin555 <jyovmuddinov@gmail.com>
2025-01-22 09:58:43 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const Username = prompt("Ismingizni kiriting")
+// const newTag = document.createElement("h1")
+// newTag.textContent = Username
+
+// newTag.style.backgroundColor = "red"
+// newTag.style.textAlign = "center"
+// newTag.style.color = "white"
+// document.body.appendChild(newTag)
+
